7862d1810e61d24a6a3a0c41417e88c94f681007
Thilina-Pathirage <thilinapathirage@gmail.com>
2021-03-26 08:34:51 +0530

    somePages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import Users from "./users/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
 
 function App() {
-  return <h1>Lets Start!</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact={true}>
+          <Users />
+        </Route>
+        <Route path="/places/new" exact={true}>
+          <NewPlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/users/components/UsersList.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './UsersList.css';
+
+const UsersList = props => {
+    return <h2>UsersList is works!</h2>
+}
+
+export default UsersList;
