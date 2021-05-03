b540c7a6655b992715ad17e0aba33b8a4e766af6
Thilina-Pathirage <thilinapathirage@gmail.com>
2021-05-03 10:15:17 +0530

    remake

--- src/App.js
+++ src/App.js
@@ -1,30 +1,10 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Redirect,
-  Switch,
-} from "react-router-dom";
-import Users from "./users/pages/Users";
-import NewPlace from "./places/pages/NewPlace";
-import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 
 function App() {
   return (
-    <Router>
-      <MainNavigation />
-      <main>
-        <Switch>
-          <Route path="/" exact={true}>
-            <Users />
-          </Route>
-          <Route path="/places/new" exact={true}>
-            <NewPlace />
-          </Route>
-          <Redirect to="/" />
-        </Switch>
-      </main>
-    </Router>
+    <div className="App">
+      <h1>Hello Destions!</h1>
+    </div>
   );
 }
 
